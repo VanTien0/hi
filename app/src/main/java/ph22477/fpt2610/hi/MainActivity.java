9b292b5246728cf4630ea0c6a90aa04b91a7f62e
tiennvph22477 <tiennvph22477@fpt.edu.vn>
2022-11-20 10:10:56 +0700

    Update

--- app/src/main/java/ph22477/fpt2610/hi/MainActivity.java
+++ app/src/main/java/ph22477/fpt2610/hi/MainActivity.java
@@ -3,12 +3,15 @@ package ph22477.fpt2610.hi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnhi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int hi9;
     }
 }
